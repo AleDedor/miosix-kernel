225a25f4efa7b4355a49c2bd8d65d1005241d4ef
Luca452 <daidoneluca98@gmail.com>
2024-02-22 20:19:57 +0100

    Added leds to main

--- main.cpp
+++ main.cpp
@@ -5,7 +5,31 @@
 using namespace std;
 using namespace miosix;
 
+typedef Gpio<GPIOA_BASE,12> ledg1;
+//typedef Gpio<GPIOA_BASE,13> ledg2;
+typedef Gpio<GPIOA_BASE,10> ledy1;
+typedef Gpio<GPIOA_BASE,11> ledy2;
+typedef Gpio<GPIOA_BASE,8> ledr1;
+typedef Gpio<GPIOA_BASE,9> ledr2;
+
+
 int main()
 {
+    ledg1::mode(Mode::OUTPUT);
+    ledy1::mode(Mode::OUTPUT);
+    ledy2::mode(Mode::OUTPUT);
+    ledr1::mode(Mode::OUTPUT);
+    ledr2::mode(Mode::OUTPUT);
+
+
+    while(1){
+        ledg1::high();
+        ledy2::high();
+        Thread::sleep(500);
+        ledg1::low();
+        ledy2::low();
+        Thread::sleep(500);
+    }
+    
     //iprintf("Hello world, write your application here\n");
 }
